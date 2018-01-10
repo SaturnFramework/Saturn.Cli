32a3aad610e9717fd74d349f21dbc943da266965
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2018-01-10 19:13:43 +0100

    Initial commit

---
+++ build.fsx
@@ -0,0 +1,159 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+#r "./packages/FAKE/tools/FakeLib.dll"
+#load "paket-files/fsharp/FAKE/modules/Octokit/Octokit.fsx"
+
+open Fake.ReleaseNotesHelper
+open Fake.AssemblyInfoFile
+open Fake.Git
+open Fake
+open System
+open Octokit
+
+// --------------------------------------------------------------------------------------
+// Information about the project to be used at NuGet and in AssemblyInfo files
+// --------------------------------------------------------------------------------------
+
+let project = "Saturn"
+
+let summary = "Opinionated, web development framework for F# which implements the server-side, functional MVC pattern"
+
+let gitOwner = "Krzysztof-Cieslak"
+let gitName = "Saturn"
+let gitHome = "https://github.com/" + gitOwner
+
+
+// --------------------------------------------------------------------------------------
+// Build variables
+// --------------------------------------------------------------------------------------
+
+let buildDir  = "./build/"
+let dotnetcliVersion = "2.1.3"
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+let release = parseReleaseNotes (IO.File.ReadAllLines "RELEASE_NOTES.md")
+
+// --------------------------------------------------------------------------------------
+// Build Targets
+// --------------------------------------------------------------------------------------
+
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir]
+)
+
+Target "AssemblyInfo" (fun _ ->
+    let getAssemblyInfoAttributes projectName =
+        [ Attribute.Title projectName
+          Attribute.Product project
+          Attribute.Description summary
+          Attribute.Version release.AssemblyVersion
+          Attribute.FileVersion release.AssemblyVersion ]
+
+    let getProjectDetails projectPath =
+        let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
+        ( projectPath,
+          projectName,
+          System.IO.Path.GetDirectoryName(projectPath),
+          (getAssemblyInfoAttributes projectName)
+        )
+
+    !! "src/**/*.??proj"
+    |> Seq.map getProjectDetails
+    |> Seq.iter (fun (projFileName, _, folderName, attributes) ->
+        match projFileName with
+        | Fsproj -> CreateFSharpAssemblyInfo (folderName </> "AssemblyInfo.fs") attributes
+        | Csproj -> CreateCSharpAssemblyInfo ((folderName </> "Properties") </> "AssemblyInfo.cs") attributes
+        | Vbproj -> CreateVisualBasicAssemblyInfo ((folderName </> "My Project") </> "AssemblyInfo.vb") attributes
+        | Shproj -> ()
+        )
+)
+
+Target "InstallDotNetCLI" (fun _ ->
+    DotNetCli.InstallDotNetSDK dotnetcliVersion |> ignore
+)
+
+Target "Restore" (fun _ ->
+    DotNetCli.Restore id
+)
+
+Target "Build" (fun _ ->
+    DotNetCli.Build id
+)
+
+// --------------------------------------------------------------------------------------
+// Release Targets
+// --------------------------------------------------------------------------------------
+
+Target "Pack" (fun _ ->
+    Paket.Pack (fun p ->
+        { p with
+            BuildConfig = "Release";
+            OutputPath = buildDir;
+            Version = release.NugetVersion
+            ReleaseNotes = String.concat "\n" release.Notes
+            MinimumFromLockFile = true
+        }
+    )
+)
+
+Target "ReleaseGitHub" (fun _ ->
+    let remote =
+        Git.CommandHelper.getGitResult "" "remote -v"
+        |> Seq.filter (fun (s: string) -> s.EndsWith("(push)"))
+        |> Seq.tryFind (fun (s: string) -> s.Contains(gitOwner + "/" + gitName))
+        |> function None -> gitHome + "/" + gitName | Some (s: string) -> s.Split().[0]
+
+    StageAll ""
+    Git.Commit.Commit "" (sprintf "Bump version to %s" release.NugetVersion)
+    Branches.pushBranch "" remote (Information.getBranchName "")
+
+    Branches.tag "" release.NugetVersion
+    Branches.pushTag "" remote release.NugetVersion
+
+    let client =
+        let user =
+            match getBuildParam "github-user" with
+            | s when not (String.IsNullOrWhiteSpace s) -> s
+            | _ -> getUserInput "Username: "
+        let pw =
+            match getBuildParam "github-pw" with
+            | s when not (String.IsNullOrWhiteSpace s) -> s
+            | _ -> getUserPassword "Password: "
+
+        createClient user pw
+
+    // release on github
+    client
+    |> createDraft gitOwner gitName release.NugetVersion (release.SemVer.PreRelease <> None) release.Notes
+    |> releaseDraft
+    |> Async.RunSynchronously
+)
+
+Target "Push" (fun _ ->
+    let key =
+        match getBuildParam "nuget-key" with
+        | s when not (String.IsNullOrWhiteSpace s) -> s
+        | _ -> getUserPassword "NuGet Key: "
+    Paket.Push (fun p -> { p with WorkingDir = buildDir; ApiKey = key }))
+
+// --------------------------------------------------------------------------------------
+// Build order
+// --------------------------------------------------------------------------------------
+Target "Default" DoNothing
+Target "Release" DoNothing
+
+"Clean"
+  ==> "InstallDotNetCLI"
+  ==> "AssemblyInfo"
+  ==> "Restore"
+  ==> "Build"
+  ==> "Default"
+
+"Default"
+  ==> "Pack"
+  ==> "ReleaseGitHub"
+  ==> "Push"
+  ==> "Release"
+
+RunTargetOrDefault "Default"
