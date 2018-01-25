a17de542a410a09039127dc2462ae0e70c728bc1
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2018-01-25 16:50:48 +0200

    Bump version to 0.1.1

--- src/Saturn.Dotnet/AssemblyInfo.fs
+++ src/Saturn.Dotnet/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Saturn.Dotnet")>]
 [<assembly: AssemblyProductAttribute("Saturn.Dotnet")>]
 [<assembly: AssemblyDescriptionAttribute("A dotnet CLI tool for Saturn projects")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Saturn.Dotnet"
     let [<Literal>] AssemblyProduct = "Saturn.Dotnet"
     let [<Literal>] AssemblyDescription = "A dotnet CLI tool for Saturn projects"
-    let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyFileVersion = "0.1.0"
+    let [<Literal>] AssemblyVersion = "0.1.1"
+    let [<Literal>] AssemblyFileVersion = "0.1.1"
