57d5097f75c59412b2f423731e71b71520b45d97
Richard Ginzburg <1675955+rickythefox@users.noreply.github.com>
2018-10-18 13:31:59 -0400

    Corrected return type of showAction in generated controller

--- src/Saturn.Dotnet/Program.fs
+++ src/Saturn.Dotnet/Program.fs
@@ -409,7 +409,7 @@ module Controller =
       let! result = Database.getById cnf.connectionString id
       match result with
       | Ok (Some result) ->
-        return result
+        return! Response.ok ctx result
       | Ok None ->
         return! Response.notFound ctx "Value not fund"
       | Error ex ->
