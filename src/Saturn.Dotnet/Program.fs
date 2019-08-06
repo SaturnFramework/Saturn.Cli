2245c0e6190b4409e12cef0ff148053ecdb4bef5
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2019-08-06 11:47:25 +1000

    🚧 Extract types to Models.fs

--- src/Saturn.Dotnet/Program.fs
+++ src/Saturn.Dotnet/Program.fs
@@ -3,55 +3,7 @@ open System.IO
 open System
 open System.Diagnostics
 
-type ParameterType =
-  | String
-  | Int
-  | Float
-  | Double
-  | Decimal
-  | Guid
-  | DateTime
-  | Bool
-with
-  static member TryParse (x: string) =
-    match x.ToLower() with
-    | "string" -> String
-    | "int" -> Int
-    | "float" -> Float
-    | "double" -> Double
-    | "decimal" -> Decimal
-    | "guid" -> Guid
-    | "datetime" -> DateTime
-    | "bool" -> Bool
-    | _ -> failwithf "Unsupported type - %s" x
-
-type Parameter = {
-  name : string
-  typ : ParameterType
-  nullable : bool
-}
-with
-  member x.FSharpType =
-    match x.typ with
-    | String -> "string"
-    | Int -> "int"
-    | Float -> "float"
-    | Double -> "double"
-    | Decimal -> "decimal"
-    | Guid -> "System.Guid"
-    | DateTime -> "System.DateTime"
-    | Bool -> "bool"
-
-  member x.DbType =
-    match x.typ with
-    | String -> "TEXT"
-    | Int -> "INT"
-    | Float -> "FLOAT"
-    | Double -> "DOUBLE"
-    | Decimal -> "DECIMAL"
-    | Guid -> "TEXT"
-    | DateTime -> "DATETIME"
-    | Bool -> "BOOLEAN"
+open Models
 
 let fsProjPath =
     let cwd = Directory.GetCurrentDirectory()
