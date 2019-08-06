7f80effc6eecfe33c527af5036368b11c96b0098
Zaymon Foulds-Cook <12402727+ZaymonFC@users.noreply.github.com>
2019-08-06 11:58:28 +1000

    🧹 Fix indentation issue in Program.fs

--- src/Saturn.Dotnet/Program.fs
+++ src/Saturn.Dotnet/Program.fs
@@ -7,37 +7,37 @@ open Models
 open Files
 
 let runMigration (extraArgv: string []) =
-  let baseArgumetns = "run --project ../Migrations/Migrations.fsproj"
-  let arguments =
-      match extraArgv with
-      | [||] -> baseArgumetns
-      | _ ->
-          sprintf "%s -- %s" baseArgumetns (Array.fold (+) "" extraArgv)
-  let startInfo = ProcessStartInfo()
-  startInfo.CreateNoWindow <- true
-  startInfo.Arguments <- arguments
-  startInfo.FileName <- "dotnet"
-  startInfo.RedirectStandardOutput <- true
-  startInfo.RedirectStandardError <- true
-  startInfo.RedirectStandardInput <- true
-  startInfo.WorkingDirectory <- Directory.GetCurrentDirectory()
-  let processs = Process.Start startInfo
-  let output = processs.StandardOutput.ReadToEnd()
-  processs.WaitForExit()
-  printfn "%s" output
+    let baseArgumetns = "run --project ../Migrations/Migrations.fsproj"
+    let arguments =
+        match extraArgv with
+        | [||] -> baseArgumetns
+        | _ ->
+            sprintf "%s -- %s" baseArgumetns (Array.fold (+) "" extraArgv)
+    let startInfo = ProcessStartInfo()
+    startInfo.CreateNoWindow <- true
+    startInfo.Arguments <- arguments
+    startInfo.FileName <- "dotnet"
+    startInfo.RedirectStandardOutput <- true
+    startInfo.RedirectStandardError <- true
+    startInfo.RedirectStandardInput <- true
+    startInfo.WorkingDirectory <- Directory.GetCurrentDirectory()
+    let processs = Process.Start startInfo
+    let output = processs.StandardOutput.ReadToEnd()
+    processs.WaitForExit()
+    printfn "%s" output
 
 let printHelp () =
-  Console.ForegroundColor <- ConsoleColor.Cyan
-  printfn "Welcome to Saturn CLI. Avaliable commands:"
-  Console.ResetColor ()
-  [
-    "* gen, gen.html", "generates the model, data access layer, controller and server side views"
-    "* gen.json", "generates the model, data access layer and controller returning data in JSON format"
-    "* gen.model", "generates the model and data access layer only"
-    "* migration", "runs all migrations, updating the database to the latest version"
-    "* interactive", "[experimental] starts interactive mode to interactively explore the running application"
-  ]
-  |> List.iter (fun (command, description) -> printfn "%-16s %s" command description)
+    Console.ForegroundColor <- ConsoleColor.Cyan
+    printfn "Welcome to Saturn CLI. Avaliable commands:"
+    Console.ResetColor ()
+    [
+        "* gen, gen.html", "generates the model, data access layer, controller and server side views"
+        "* gen.json", "generates the model, data access layer and controller returning data in JSON format"
+        "* gen.model", "generates the model and data access layer only"
+        "* migration", "runs all migrations, updating the database to the latest version"
+        "* interactive", "[experimental] starts interactive mode to interactively explore the running application"
+    ]
+    |> List.iter (fun (command, description) -> printfn "%-16s %s" command description)
 
 [<EntryPoint>]
 let main argv =
@@ -47,20 +47,20 @@ let main argv =
             let flags, argv = argv |> Array.partition (fun f -> f.StartsWith "--")
             match action with
             | "gen" | "gen.html" ->
-              let name = argv.[1]
-              let names = argv.[2]
-              let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
-              CodeGeneration.generateHtml name names fields
+                let name = argv.[1]
+                let names = argv.[2]
+                let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
+                CodeGeneration.generateHtml name names fields
             | "gen.json" ->
-              let name = argv.[1]
-              let names = argv.[2]
-              let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
-              CodeGeneration.generateJson name names fields
+                let name = argv.[1]
+                let names = argv.[2]
+                let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
+                CodeGeneration.generateJson name names fields
             | "gen.model" ->
-              let name = argv.[1]
-              let names = argv.[2]
-              let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
-              CodeGeneration.generateMdl name names fields
+               let name = argv.[1]
+               let names = argv.[2]
+               let fields = argv.[3 ..] |> Array.map (fun n -> let x = n.Split(':', 2) in {name = x.[0]; typ = ParameterType.TryParse x.[1]; nullable = false})
+               CodeGeneration.generateMdl name names fields
             // | "gen.graphql" -> generateGraphQL name names fields
             | "migration" -> runMigration argv.[1 ..]
             | "interactive" -> Interactive.start (Directory.GetCurrentDirectory())
